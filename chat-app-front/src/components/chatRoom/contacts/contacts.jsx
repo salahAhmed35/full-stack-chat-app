9019669a5fd5da23dec258e446ad772913055db2
salahAhmed <salahkobia13@gmail.com>
2024-02-14 19:58:47 +0200

    add style to chats list

--- chat-app-front/src/components/chatRoom/contacts/contacts.jsx
+++ chat-app-front/src/components/chatRoom/contacts/contacts.jsx
@@ -3,11 +3,11 @@ import "./contacts.css";
 const Contacts = () => {
   return (
     <React.Fragment>
-      <div className="contacts p-5 bg-[#f0f9ff]">
+      <div className="contacts py-5 px-2 bg-[#f0f9ff]">
         <h3 className="text-4xl font-bold text-slate-700 mb-5">Chats</h3>
         <div className="chats">
           <ul>
-            <li className="flex justify-between items-center py-2 w-100 cursor-pointer">
+            <li className="flex justify-between  p-3 w-100 cursor-pointer border-b border-white-400 select-none">
               <div className="flex flex-col">
                 <p className="font-bold text-gray-700 text-lg">Name of user</p>
                 <div className="last-message">
@@ -15,7 +15,7 @@ const Contacts = () => {
                 </div>
               </div>
               <div className="message-time">
-                <span>02:50 PM</span>
+                <span className="text-sm text-gray-500">02:50 PM</span>
               </div>
             </li>
           </ul>
